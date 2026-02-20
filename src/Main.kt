453609b563ce5e2507663ccd27ecd5e4ce8513b1
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-21 01:01:34 +0300

    Code Snippet 6

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,5 @@
 fun main() {
 
-    val name = "John Sinclair" // Type Inference (String)
-    val regularNumber = 10 // Int
-    val longNumber = 10L // Long
-    val doubleNumber = 10.0 // Double
-    val floatNumber = 10F // Float
+    val intNumber = 44
+    val longNumber: Long = intNumber.toLong()
 }
