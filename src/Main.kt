bb8bf4d442218c983bb5a740d01f3aa1b2e8270d
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-21 00:50:28 +0300

    Code Snippet 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,13 @@
-const val PI = 3.14
-
 fun main() {
-    var name: String = "John Sinclair"
-    var age: Int = 30
-    println("$name is $age years old")
-    name = "John Sinclair Jr." // 1
-    age = 12
-    println("$name is $age years old")
+
+    val b: Byte = 127 // 8bit
+    val bm: Byte = -128 // 8bit
+    val s: Short = 10000 // 16bit
+    val i: Int = 1000000000 // 32bit
+    val l: Long = 1000000000L // 64bit
+    val f: Float = 100.0F // 32bit
+    val d: Double = 100.0 // 64bit
+    val c: Char = 'm' // 16bit
+    val bo: Boolean = true // 1bit
+    val st: String = "Mahmoud" // N/A Bit
 }
