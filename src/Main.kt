0339fbd2c5113ba3436f63c4883b3847705ba503
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-21 00:56:02 +0300

    Code Snippet 5

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,8 @@
 fun main() {
 
-    val b: Byte = 127 // 8bit
-    val bm: Byte = -128 // 8bit
-    val s: Short = 10000 // 16bit
-    val i: Int = 1000000000 // 32bit
-    val l: Long = 1000000000L // 64bit
-    val f: Float = 100.0F // 32bit
-    val d: Double = 100.0 // 64bit
-    val c: Char = 'm' // 16bit
-    val bo: Boolean = true // 1bit
-    val st: String = "Mahmoud" // N/A Bit
+    val name = "John Sinclair" // Type Inference (String)
+    val regularNumber = 10 // Int
+    val longNumber = 10L // Long
+    val doubleNumber = 10.0 // Double
+    val floatNumber = 10F // Float
 }
