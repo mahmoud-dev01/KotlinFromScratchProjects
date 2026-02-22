10e574f42bae719e413d4c94b9e7254572886aca
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 00:56:27 +0300

    exercise 1 done

--- src/exercises/Exercise1.kt
+++ src/exercises/Exercise1.kt
@@ -1,2 +1,21 @@
 package exercises
 
+fun main() {
+
+    println("Enter your age:")
+    val age: Int = readln().toInt()
+    if (age < 18) {
+        println("You are not eligible to vote yet.")
+    } else if (age >= 18 && age <= 120) {
+        println("You are eligible to vote.")
+    } else {
+        println("Please enter a valid age.")
+    }
+
+    println("Enter your age:")
+    when(readln().toInt()) {
+        in 0 .. 17 -> println("You are not eligible to vote yet.")
+        in 18 .. 120 -> println("You are eligible to vote.")
+        else -> println("Please enter a valid age.")
+    }
+}
