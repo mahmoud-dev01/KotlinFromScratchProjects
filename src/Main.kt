7b449385688a580596e6cc875f6aecf65f4d6069
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 00:10:25 +0300

    new package for exercises

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,3 @@
 fun main() {
-    val x: Int? = readlnOrNull()?.toInt() ?: 0
-    when {
-        x!! > 0 -> println("x is positive")
-        x!! == 0 -> println("x is zero")
-        x!! < 0 -> println("x is negative")
-        else -> println("x is not real number")
-    }
 
-    val hour = readln().toInt()
-    when (hour) {
-        in 0 .. 11 -> println("Good Morning")
-        in 12 .. 16 -> println("Good Afternoon")
-        in 17 .. 23 -> println("Good Evening")
-        else -> println("Invalid hour")
-    }
 }
