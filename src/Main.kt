6ee17468af3b7649c860947d55b71b76a26b089a
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 00:08:08 +0300

    Code Snippet 12

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,17 @@
 fun main() {
-    val x = 10
-    if (x > 0) println("x is positive") else println("x is negative")
-
-    val y = -10
-    if (x > 0) {
-        println("y is positive")
-    } else {
-        println("y is negative")
+    val x: Int? = readlnOrNull()?.toInt() ?: 0
+    when {
+        x!! > 0 -> println("x is positive")
+        x!! == 0 -> println("x is zero")
+        x!! < 0 -> println("x is negative")
+        else -> println("x is not real number")
     }
 
-    val a = 100
-    val b = -30
-    var max: Int = 0
-    if (a > b) {
-        max = a
-        println("a is greater than b.")
-        println("max of $a and b is: $max")
-    } else if (a < b) {
-        max = b
-        println("b is greater than a.")
-        println("max of $a and b is: $max")
-    } else {
-        println("a and b have the same value: $a")
+    val hour = readln().toInt()
+    when (hour) {
+        in 0 .. 11 -> println("Good Morning")
+        in 12 .. 16 -> println("Good Afternoon")
+        in 17 .. 23 -> println("Good Evening")
+        else -> println("Invalid hour")
     }
-    println(max)
 }
