1f78cf65216b751dbd09beb7978c16ceadd9c518
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 01:36:41 +0300

    Code Snippet 14

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,20 @@
 fun main() {
 
-    for (index: Int in 0 .. 4) {
-        println("Current value of index is: $index")
-        if (index == 4) println("-----")
-    }
-    for (index: Int in 0 until 5 /*step 2*/) {
-        println("Current value of index is: $index")
-        if (index == 4) println("-----")
-    }
-    for (index: Int in 5 downTo 0)
-        println("Current value of index: $index")
-
-    for (i in 1 .. 4) {
-        if (i == 3) continue
-        println("index: $i")
+    val size: Int = 4
+    Line@ for (i in 1 .. size) {
+        Star@ for (j in 1 .. size) {
+            print("* ")
+        }
+        println()
     }
 
-    for (i in 1 .. 4) {
-        if (i == 3) break
-        println("index: $i")
+    Loop1@ for (i in 1 .. 5) {
+        Loop2@ for (j in 1 .. 5) {
+            print("$i,$j ")
+            if (i == j) {
+                println()
+                continue@Loop1
+            }
+        }
     }
 }
