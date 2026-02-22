f3723a989597112848b707091f46ce2901eba46f
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 01:53:01 +0300

    new exercise 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,3 @@
 fun main() {
 
-    var count = 0
-    while (count < 4) {
-        println("current value of count: $count")
-        count++
-    }
-
-    count = 0
-    while (true) {
-        println("play: $count")
-        when(readln().toInt()) {
-            0 -> break
-            1 -> continue
-            else -> {
-                count++
-            }
-        }
-    }
-
-    count = 0
-    do {
-        count++
-        println(count)
-    } while (count == 1)
 }
