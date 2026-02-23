8e9d90562d2f0a8397909c7cc2445cb366f9febc
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-23 15:18:24 +0300

    exercise 2 done

--- src/exercises/Exercise2.kt
+++ src/exercises/Exercise2.kt
@@ -1,2 +1,30 @@
 package exercises
 
+fun main() {
+
+    val n: Int = readln().toInt()
+    for (i in 1..n) {
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+    }
+
+    val input: String = readln()
+    var first: Int = 0
+    var last: Int = input.length - 1
+    var isMatch: Boolean = true
+
+    while (first < last) {
+        if (input[first] != input[last]) {
+            println("Not a palindrome")
+            isMatch = false
+            break
+        }
+        last--
+        first++
+    }
+    if (isMatch) {
+        println("Palindrome")
+    }
+}
