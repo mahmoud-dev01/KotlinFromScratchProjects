fffb1324fc23aa93e04fd3aba74f256e8efc7504
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-27 17:47:55 +0300

    new exercise 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,3 @@
 fun main() {
 
-    val greet: (String) -> String = { name -> "Hello $name!" }
-
-    println(greet("Alica"))
-    println(greet("Bob"))
-    println(
-        apply { name ->
-            greet(name)
-        }("Mahmoud")
-    )
-}
-fun apply(func: (String) -> String): (String) -> String {
-    return { name -> func(name) }
 }
