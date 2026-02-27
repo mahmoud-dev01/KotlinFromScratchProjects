13465cd31adb10351571ea00d81676bbc015f1b8
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-02-27 18:24:20 +0300

    done exercise 3

--- src/exercises/Exercise3.kt
+++ src/exercises/Exercise3.kt
@@ -1,2 +1,16 @@
 package exercises
 
+val max: (Int, Int) -> Int = { a, b -> if (a >= b) a else b }
+
+val newStringReversed: (String) -> String? = { text ->
+    var reversed: String = ""
+    for (index in text.indices) {
+        reversed += text[(text.length -1) - index]
+    }
+    reversed
+}
+
+fun main() {
+    println(max(2, 3))
+    println(newStringReversed("Kotlin"))
+}
