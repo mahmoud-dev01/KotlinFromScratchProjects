a3e62da959549b910ced76d61276548fe7744d95
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-03-01 03:59:40 +0300

    Code Snippet 19

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
 
+    println("Enter some text:")
+    val userInput = readln()
+    println("You entered: $userInput")
+
+    while (true) {
+        println("Enter an integer:")
+        val num = readln()
+        try {
+            val intValue = num.toInt()
+            println("You entered: $intValue")
+            break
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Try again.")
+        }
+    }
 }
