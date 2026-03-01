ebdde4c0c69607c148c277a90e7c22b3b4c7cbc4
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2026-03-01 11:52:36 +0300

    Project 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,3 @@
-import java.io.File
-import java.util.Scanner
-
 fun main() {
 
-    val inputFile = File("inputfile.txt")
-    val outputFile = File("outputfile.txt")
-    try {
-        val scanner = Scanner(inputFile)
-        while (scanner.hasNextLine()) {
-            val line = scanner.nextLine()
-            println(line)
-        }
-    } catch (e: Exception) {
-        println("An error occurred: ${e.message}")
-    }
-   try {
-       val lines = inputFile.readLines()
-       for (line: String in lines) {
-           outputFile.appendText("$line\n")
-       }
-       println("Copied inputfile.txt to outputfile.txt")
-   } catch (e: Exception) {
-       println("An error occurred: ${e.message}")
-   }
 }
